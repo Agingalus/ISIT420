a6aa1530876804557b296a1df71c0eea18101caf
Aaron Greene <agingalus613@gmail.com>
2021-05-04 14:26:01 -0700

    new project

---
+++ Order500HW3/Order500HW3/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Order500HW3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.employeeButton = new System.Windows.Forms.Button();
+            this.storeSelect = new System.Windows.Forms.Button();
+            this.employeeList = new System.Windows.Forms.ComboBox();
+            this.storeList = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // employeeButton
+            // 
+            this.employeeButton.Location = new System.Drawing.Point(133, 85);
+            this.employeeButton.Name = "employeeButton";
+            this.employeeButton.Size = new System.Drawing.Size(132, 23);
+            this.employeeButton.TabIndex = 0;
+            this.employeeButton.Text = "Select Employee";
+            this.employeeButton.UseVisualStyleBackColor = true;
+            this.employeeButton.Click += new System.EventHandler(this.employeeButton_Click);
+            // 
+            // storeSelect
+            // 
+            this.storeSelect.Location = new System.Drawing.Point(590, 104);
+            this.storeSelect.Name = "storeSelect";
+            this.storeSelect.Size = new System.Drawing.Size(75, 23);
+            this.storeSelect.TabIndex = 1;
+            this.storeSelect.Text = "Select Store";
+            this.storeSelect.UseVisualStyleBackColor = true;
+            this.storeSelect.Click += new System.EventHandler(this.storeSelect_Click);
+            // 
+            // employeeList
+            // 
+            this.employeeList.Location = new System.Drawing.Point(133, 147);
+            this.employeeList.Name = "employeeList";
+            this.employeeList.Size = new System.Drawing.Size(121, 21);
+            this.employeeList.TabIndex = 4;
+            this.employeeList.SelectedIndexChanged += new System.EventHandler(this.employeeList_SelectedIndexChanged);
+            // 
+            // storeList
+            // 
+            this.storeList.FormattingEnabled = true;
+            this.storeList.Location = new System.Drawing.Point(616, 168);
+            this.storeList.Name = "storeList";
+            this.storeList.Size = new System.Drawing.Size(121, 21);
+            this.storeList.TabIndex = 3;
+            this.storeList.SelectedIndexChanged += new System.EventHandler(this.storeList_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 540);
+            this.Controls.Add(this.storeList);
+            this.Controls.Add(this.employeeList);
+            this.Controls.Add(this.storeSelect);
+            this.Controls.Add(this.employeeButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button employeeButton;
+        private System.Windows.Forms.Button storeSelect;
+        private System.Windows.Forms.ComboBox employeeList;
+        private System.Windows.Forms.ComboBox storeList;
+    }
+}
+
---
+++ Order500HW3/Order500HW3/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Order500HW3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_load(object sender, EventArgs e)
+        {
+            NodeOrders500Entities nodeOrders500 = new NodeOrders500Entities();
+            employeeList.Items.Add("hi");
+            employeeList.Items.Add("buy");
+
+        }
+
+
+
+        private void employeeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void storeSelect_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void employeeList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            employeeList.Items.Add(2);
+            employeeList.Items.Add(1);
+
+
+        }
+
+        private void storeList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+ 
+    }
+}
